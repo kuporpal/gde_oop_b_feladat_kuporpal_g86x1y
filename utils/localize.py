6a3d73ca49b2b462eb2cd7724977ee6ff39d31a9
kuporpal <kuporpal@gmail.com>
2026-05-17 17:19:27 +0200

    Update localize.py

--- utils/localize.py
+++ utils/localize.py
@@ -8,16 +8,24 @@ class Localize:
                 'menu_help': 'Súgó',
                 'menu_file': 'Fájl',
                 'menu_exit': 'Kilépés',
-                'aboutmessage': 'Objektum Orientált Programozás \'B\' Beadandó Feladat\nRepülőjegy Foglalási Rendszer\nA programot készítette: Kupor Pál\nNeptun kód: G86X1Y',
-                'windowtitle': 'Repülőjegy Foglalási Rendszer'
+                'about_message': 'Objektum Orientált Programozás \'B\' Beadandó Feladat\nRepülőjegy Foglalási Rendszer\nA programot készítette: Kupor Pál\nNeptun kód: G86X1Y',
+                'window_title': 'Repülőjegy Foglalási Rendszer',
+                'exit_button': ' Kilépés a programból',
+                'exit_alert': 'Biztosan be akarja zárni a programot?',
+                'international_flight': 'Nemzetközi Járat',
+                'domestic_flight': 'Belföldi Járat'
             },
             'en': {
                 'menu_about': 'About',
                 'menu_help': 'Help',
                 'menu_file': 'File',
                 'menu_exit': 'Exit',
-                'aboutmessage': 'Object-Oriented Programming Assignment \'B\'\nFlight Ticket Booking System\nCreated by: Pál Kupor\nNeptun code: G86X1Y',
-                'windowtitle': 'Flight Ticket Booking System'
+                'about_message': 'Object-Oriented Programming Assignment \'B\'\nFlight Ticket Booking System\nCreated by: Pál Kupor\nNeptun code: G86X1Y',
+                'window_title': 'Flight Ticket Booking System',
+                'exit_button': ' Exit from the booking system',
+                'exit_alert': 'Are You sure to exit the program?',
+                'international_flight': 'International Flight',
+                'domestic_flight': 'Domestic Flight'
             }
         }
 
